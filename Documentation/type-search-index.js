af27a2476937170869f089ba6fb7ee6d292eb9f0
Tahmid Zahin <tzahin10@gmail.com>
2022-02-11 06:33:29 +0600

    adding UI screenshot

---
+++ Documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"elevator_control.Central_Controller"},{"p":"<Unnamed>","l":"elevator_control"},{"p":"<Unnamed>","l":"elevator_control.Local_Controller"}];updateSearchResults();
